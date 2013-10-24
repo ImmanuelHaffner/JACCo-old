97088ab55293771be5a81251a9bc96517d83bfd7
Immanuel Haffner <s9imhaff@stud.uni-saarland.de>
2013-10-24 19:26:54 +0200

    added a file that introduces the C4 namespace

---
+++ src/C4.h
@@ -0,0 +1,12 @@
+//===--- C4.h ----------------------------------------------------------===//
+//
+//	~~~ The C4 Compiler ~~~
+//
+//	This file defines and describes the C4 Compiler namespace.
+//
+//===----------------------------------------------------------------------===//
+
+/// \addtogroup C4
+///
+/// The C4 Compiler namespace
+namespace C4 {};
