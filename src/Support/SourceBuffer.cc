5aade5277731a53bc98730542eb1917c44fcb05c
Immanuel Haffner <s9imhaff@stud.uni-saarland.de>
2013-10-25 15:32:02 +0200

    refactored SourceBuffer, constructor now takes a file name, not FILE*; method init now opens, reads and closes the FILE specified by the file name

--- src/Support/SourceBuffer.cc
+++ src/Support/SourceBuffer.cc
@@ -10,12 +10,16 @@
 
 #include <cassert>
 #include <iostream>
+#include <cstdio>
+#include "../pos.h"
+#include "../diagnostic.h"
 
 using namespace C4;
 
-SourceBuffer::SourceBuffer( FILE *file ) : initialized(false), file(file)
+SourceBuffer::SourceBuffer( char const * const fileName ) :
+  initialized(false), fileName(fileName)
 {
-	assert( file != NULL && "file must not be NULL" );
+	assert( fileName != NULL && "fileName must not be NULL" );
 }
 
 SourceBuffer::~SourceBuffer()
@@ -25,18 +29,35 @@ SourceBuffer::~SourceBuffer()
 
 void SourceBuffer::init()
 {
+  FILE * file = NULL;
+  if ( strEq( "<stdin>", fileName ) )
+  {
+    file = stdin;
+  }
+  else
+    file = fopen( fileName, "rb" );
+
+  if ( ! file )
+    errorErrno( Pos( fileName ) );
+
 	Buffer = new std::vector<char>();
 
 	int c;
 	while ( (c = getc( file )) != EOF )
 		Buffer->push_back( c );
 
+  fclose ( file );
 	initialized = true;
 }
 
 bool SourceBuffer::isInitialized() const
 {
 	return initialized;
+}
+
+char const * SourceBuffer::getSourceFileName() const
+{
+  return fileName;
 }
 
 size_t SourceBuffer::getSize() const
@@ -64,7 +85,28 @@ std::vector<char>::const_iterator SourceBuffer::getBufEnd() const
 	return Buffer->end();
 }
 
-bool SourceBuffer::isBufEnd( std::vector<char>::iterator &it ) const
+bool SourceBuffer::isBufEnd( std::vector<char>::iterator const &it ) const
 {
 	return it == Buffer->end();
+}
+
+bool SourceBuffer::isBufEnd( std::vector<char>::const_iterator const &it ) const
+{
+	return it == Buffer->end();
+}
+
+void SourceBuffer::dump() const
+{
+  std::cout << fileName << ":" << std::endl;
+
+  if ( ! initialized )
+  {
+    std::cout << "\tnot initialized";
+  }
+
+  for ( auto it = getBufStart(); ! isBufEnd( it ); ++it )
+  {
+    std::cout << *it;
+  }
+  std::cout << std::endl;
 }
--- src/Support/SourceBuffer.h
+++ src/Support/SourceBuffer.h
@@ -12,25 +12,32 @@
 #include <cstdio>
 #include <vector>
 
-namespace C4{
+namespace C4
+{
 	class SourceBuffer
 	{
 		public:
-			SourceBuffer( FILE * file );
+			SourceBuffer( char const * const fileName );
 			~SourceBuffer();
 
+      /// Opens the file specified by <i>fileName<\i> and reads every single
+      /// character into a buffer.
+      /// Afterwards, the file is closed.
 			void init();
 			bool isInitialized() const;
+      char const * getSourceFileName() const;
 			size_t getSize() const;
 			std::vector<char>::iterator getBufStart();
 			std::vector<char>::iterator getBufEnd();
 			std::vector<char>::const_iterator getBufStart() const;
 			std::vector<char>::const_iterator getBufEnd() const;
-			bool isBufEnd( std::vector<char>::iterator &it ) const;
+			bool isBufEnd( std::vector<char>::iterator const &it ) const;
+			bool isBufEnd( std::vector<char>::const_iterator const &it ) const;
+      void dump() const;
 
 		private:
 			bool initialized;
-			FILE *file;
+      char const * const fileName;
 			std::vector<char> *Buffer;
 	};
 }
