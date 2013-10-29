e6fbf64def5cebaacd9a3643df9419aee0bf1992
Immanuel Haffner <s9imhaff@stud.uni-saarland.de>
2013-10-29 17:49:19 +0100

    adapted main according to the Lexer constructor

--- src/main.cc
+++ src/main.cc
@@ -8,60 +8,61 @@
 #include "Support/Diagnostic.h"
 
 enum class Mode {
-	TOKENIZE,
-	PARSE,
-	PRINT_AST,
-	COMPILE,
+  TOKENIZE,
+  PARSE,
+  PRINT_AST,
+  COMPILE,
 };
 
 int main(int, char** const argv)
 {
-	try {
-		char** i = argv + 1;
+  try {
+    char** i = argv + 1;
 
-		Mode mode = Mode::COMPILE;
-		for (; auto const arg = *i; ++i) {
-			if (arg[0] != '-') {
-				break;
-			} else if (strEq(arg, "--tokenize")) {
-				mode = Mode::TOKENIZE;
-			} else if (strEq(arg, "--parse")) {
-				mode = Mode::PARSE;
-			} else if (strEq(arg, "--print-ast")) {
-				mode = Mode::PRINT_AST;
-			} else if (strEq(arg, "--compile")) {
-				mode = Mode::COMPILE;
-			} else if (strEq(arg, "-")) {
-				break;
-			} else if (strEq(arg, "--")) {
-				++i;
-				break;
-			} else {
-				errorf("unknown argument '%s'", arg);
-			}
-		}
+    Mode mode = Mode::COMPILE;
+    for (; auto const arg = *i; ++i) {
+      if (arg[0] != '-') {
+        break;
+      } else if (strEq(arg, "--tokenize")) {
+        mode = Mode::TOKENIZE;
+      } else if (strEq(arg, "--parse")) {
+        mode = Mode::PARSE;
+      } else if (strEq(arg, "--print-ast")) {
+        mode = Mode::PRINT_AST;
+      } else if (strEq(arg, "--compile")) {
+        mode = Mode::COMPILE;
+      } else if (strEq(arg, "-")) {
+        break;
+      } else if (strEq(arg, "--")) {
+        ++i;
+        break;
+      } else {
+        errorf("unknown argument '%s'", arg);
+      }
+    }
 
-		if (!*i)
-			errorf("no input files specified");
+    if (!*i)
+      errorf("no input files specified");
 
-		if (!hasNewErrors()) {
-			for (; char const *name = *i; ++i) {
-				FILE* f;
-				if (strEq(name, "-")) {
-					f    = stdin;
-					name = "<stdin>";
-				} else {
-					f = fopen(name, "rb");
-					if (!f)
-						errorErrno(Pos(name));
-				}
+    if (!hasNewErrors()) {
+      for (; char const *name = *i; ++i) {
+        FILE* f;
+        if (strEq(name, "-")) {
+          f    = stdin;
+          name = "<stdin>";
+        } else {
+          f = fopen(name, "rb");
+          if (!f)
+            errorErrno(Pos(name));
+          fclose( f );
+        }
 
-				if (hasNewErrors())
-					continue;
+        if (hasNewErrors())
+          continue;
 
-				switch (mode) {
-					case Mode::TOKENIZE:
-						{
+        switch (mode) {
+          case Mode::TOKENIZE:
+            {
               //buf.init();
               //C4::Lex::Lexer Lexer(buf);
               //
@@ -71,26 +72,26 @@ int main(int, char** const argv)
               //tok->dump();
               //}
 
-							break;
-						}
+              break;
+            }
 
-					case Mode::PARSE:
-						{}
-					case Mode::PRINT_AST:
-						{}
-					case Mode::COMPILE:
-						{
-							PANIC("TODO implement");
-						}
-				}
-        fclose ( f );
-			}
-		}
-	} catch (std::exception const& e) {
-		errorf("caught exception: %s", e.what());
-	} catch (...) {
-		errorf("caught unknown exception");
-	}
+          case Mode::PARSE:
+            {}
+          case Mode::PRINT_AST:
+            {}
+          case Mode::COMPILE:
+            {
+              PANIC("TODO implement");
+            }
+        }
+        //fclose ( f );
+      }
+    }
+  } catch (std::exception const& e) {
+    errorf("caught exception: %s", e.what());
+  } catch (...) {
+    errorf("caught unknown exception");
+  }
 
-	return printDiagnosticSummary();
+  return printDiagnosticSummary();
 }
