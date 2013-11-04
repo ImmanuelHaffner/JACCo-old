0b0d436b21ce1d89d1a1b0c88c05ceb1cbb06dc3
Immanuel Haffner <s9imhaff@stud.uni-saarland.de>
2013-11-04 20:38:44 +0100

    added automated removal of test cases containing floating points to the test suite script

--- test/test.sh
+++ test/test.sh
@@ -13,6 +13,14 @@ do
   do
     echo "testing $path/${test}";
 
+# contains floating point
+    $(grep -e '\.[0-9]' -e '[0-9]\.' "$test" > /dev/null)
+    if [ $? -eq 0 ];
+    then
+      rm "$test"
+      continue
+    fi
+
     testname=$(basename "${test}" .c);
     result=$(mktemp /tmp/"${testname}".XXXX);
     expectedfile=$(dirname "${test}")/"${testname}"".expected"
