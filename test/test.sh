0df218b7284c138bb9a4e316a234c54914849061
Immanuel Haffner <s9imhaff@stud.uni-saarland.de>
2013-11-04 17:03:31 +0100

    improved test script

--- test/test.sh
+++ test/test.sh
@@ -2,46 +2,50 @@
 
 export C4="/home/immanuel/Documents/University/Compiler Construction/c4/build/default/c4";
 
-for test in $(find "resource/" -iname "*.c" -type f);
+for path in $(find "resource/" -type d);
 do
-  echo "testing ${test}";
-  oldcwd=$(pwd);
+  oldcwd=$(pwd)
+  cd "$path"
 
-  testname=$(basename "${test}" .c);
-  result=$(mktemp /tmp/"${testname}".XXXX);
-  expectedfile=$(dirname "${test}")/"${testname}".expected
-  expected=$(mktemp /tmp/"${testname}".expected.XXXX);
-  tail -n +2 "${expectedfile}" > "${expected}";
-  
-  cd $(dirname "${test}");
-  $("${C4}" --tokenize "$(basename ${test})" > "${result}" 2>&1)
-  RES=$?
-  cd "${oldcwd}";
+  for test in $(ls *.c)
+  do
+    echo "testing ${test}";
 
-  EXP=$(head -n 1 ${expectedfile})
-  if [ $EXP -ne 0 ]
-  then
-    if [ $RES -ne 0 ]
+    testname=$(basename "${test}" .c);
+    result=$(mktemp /tmp/"${testname}".XXXX);
+    expectedfile=$(dirname "${test}")/"${testname}"".expected"
+    expected=$(mktemp /tmp/"${testname}".expected.XXXX);
+    tail -n +2 "${expectedfile}" > "${expected}";
+
+    $("${C4}" --tokenize "$(basename ${test})" > "${result}" 2>&1)
+    RES=$?
+    EXP=$(head -n 1 ${expectedfile})
+
+    if [ $EXP -ne 0 ];
     then
-      echo "-> passed";
-      continue;
-    fi
-    rm "${result}";
-    cd "${oldcwd}";
-    echo "-> error";
-    continue;
-  fi;
+      if [ $RES -ne 0 ];
+      then
+        echo "-> passed";
+        continue;
+      fi
 
-  $(diff -q "${result}" "${expected}" > /dev/null 2>&1)
-  if [ 0 -ne $? ];
-  then
-    diff -y "${result}" "${expected}"
-    echo "-> output differs: ${test}";
-    exit 1;
-  else
-    echo "-> passed";
-  fi;
-  
-  rm "${result}";
-  rm "${expected}";
+      rm "${result}";
+      echo "-> error";
+      continue;
+    fi;
+
+    $(diff -q "${result}" "${expected}" > /dev/null 2>&1)
+    if [ 0 -ne $? ];
+    then
+      diff -y "${result}" "${expected}"
+      echo "-> output differs: ${test}";
+      exit 1;
+    else
+      echo "-> passed";
+    fi;
+
+    rm "${result}";
+    rm "${expected}";
+  done
+  cd "$oldcwd"
 done
