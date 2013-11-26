4e678a653d64a160f5c2f90476be84ceafb095e9
Maximilian Harz <maxharz@gmail.com>
2013-11-27 00:25:47 +0100

    ASTNode interface für visitor pattern hinzugefügt

---
+++ src/AST/ASTNode.h
@@ -0,0 +1,33 @@
+
+//===--- ASTNode.h --------------------------------------------------------===//
+//
+//	~~~ The C4 Compiler ~~~
+//
+//	This file defines the Abstract Syntax Tree interface.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef C4_ASTNODE_H
+#define C4_ASTNODE_H
+
+#include <iostream>
+
+
+namespace C4
+{
+  namespace AST
+  {
+    ///
+    struct ASTNode 
+    {
+      virtual void accept ( ASTNodeVisitor );
+      private:
+      String printed;
+    }; 
+
+    std::ostream & operator<<( std::ostream &out,
+        BinaryExpression const &expr );
+  } // end namespace AST
+} // end namespace C4
+
+#endif
