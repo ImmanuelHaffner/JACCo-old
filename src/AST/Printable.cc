a46ce626caa54b5302bd18118d8c6bddefe532c1
Maximilian Harz <s9maharz@stud.uni-saarland.de>
2013-12-02 18:47:41 +0100

    changed format of printable.cc

--- src/AST/Printable.cc
+++ src/AST/Printable.cc
@@ -16,103 +16,103 @@ using namespace AST;
 
 void Variable::print( Printer const p ) const
 {
-    p.out << this->tok.sym;
+  p.out << this->tok.sym;
 }
 
 void BinaryExpr::print( Printer const p ) const
 {
-    this->lhs.print( p );
-    p.out << " " << this->tok.sym << " "; 
-    this->rhs.print( p );
+  this->lhs.print( p );
+  p.out << " " << this->tok.sym << " "; 
+  this->rhs.print( p );
 }
 
 void PostIncExpr::print( Printer const p ) const
 {
-    this->expr.print( p );
-    p.out << "++";
+  this->expr.print( p );
+  p.out << "++";
 }
 
 void PostDecExpr::print( Printer const p ) const
 {
-    this->expr.print( p );
-    p.out << "--";
+  this->expr.print( p );
+  p.out << "--";
 }
 
 void ArrowExpr::print( Printer const p ) const
 {
-    this->lhs.print( p );
-    p.out << "->" << this->tok.sym;
+  this->lhs.print( p );
+  p.out << "->" << this->tok.sym;
 }
 
 void DotExpr::print( Printer const p ) const
 {
-    this->lhs.print( p );
-    p.out << "." << this->tok.sym;
+  this->lhs.print( p );
+  p.out << "." << this->tok.sym;
 }
 
 void AssignmentExpr::print( Printer const p ) const
 {
-    this->lhs.print( p );
-    p.out << " " << this->tok.sym << " ";
-    this->rhs.print( p );
+  this->lhs.print( p );
+  p.out << " " << this->tok.sym << " ";
+  this->rhs.print( p );
 }
 
 void ConditionalExpr::print( Printer const p ) const
 {
-    this->cond.print( p );
-    p.out << " ? ";
-    this->lhs.print( p );
-    p.out << " : ";
-    this->rhs.print ( p );
+  this->cond.print( p );
+  p.out << " ? ";
+  this->lhs.print( p );
+  p.out << " : ";
+  this->rhs.print ( p );
 }
 
 void Constant::print( Printer const p ) const
 {
-    p.out << this->tok.sym;
+  p.out << this->tok.sym;
 }
 
 void PreDecExpr::print( Printer const p ) const
 {
-    p.out << "--";
-    this->expr.print( p );
+  p.out << "--";
+  this->expr.print( p );
 }
 
 void PreIncExpr::print( Printer const p ) const
 {
-    p.out << "++";
-    this->expr.print( p );
+  p.out << "++";
+  this->expr.print( p );
 }
 
 void FunctionCall::print( Printer const p ) const
 {
-    //TODO 
-    (void) p;
+  //TODO 
+  (void) p;
 }
 
 void SizeofExpr::print( Printer const p ) const
 {
-    p.out << this->tok.sym << " ";
-    this->expr.print( p );
+  p.out << this->tok.sym << " ";
+  this->expr.print( p );
 }
 
 void SizeofTypeExpr::print( Printer const p ) const
 {
-    //TODO: Missing << / print() for types
-    /*p.out << this->tok.sym << "(";
-      this->type.print( p );
-      p.out << ")";*/
-    (void) p;
+  //TODO: Missing << / print() for types
+  /*p.out << this->tok.sym << "(";
+    this->type.print( p );
+    p.out << ")";*/
+  (void) p;
 
 }
 
 void SubscriptExpr::print( Printer const p ) const
 {
-    p.out << this->expr << "[";
-    this->index.print( p );
-    p.out << "]";
+  p.out << this->expr << "[";
+  this->index.print( p );
+  p.out << "]";
 }
 
 void Type::print( Printer const p ) const
 {
-    p.out << this->tok.sym;
+  p.out << this->tok.sym;
 }
