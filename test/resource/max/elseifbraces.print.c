bea99178cd60d8e1cd165c012c78d1947bef13c8
Maximilian Harz <s9maharz@stud.uni-saarland.de>
2013-12-12 17:42:38 +0100

    now really added the mentioned tests, plus one about expressions

---
+++ test/resource/max/elseifbraces.print.c
@@ -0,0 +1,10 @@
+int (f(int x, int y))
+{
+		if (1)
+			return 2;
+		else if (a + b)
+    {
+			return 1;
+    } else
+      return 3;
+}
---
+++ test/resource/max/simpleIf.print.c
@@ -0,0 +1,7 @@
+int (f(int x, int y))
+{
+		if (1)
+			return 2;
+		else
+			return 1;
+}
