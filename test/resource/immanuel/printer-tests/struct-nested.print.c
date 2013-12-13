4ed845c98d1202aadb6bac24158356159a625d64
Immanuel Haffner <s9imhaff@stud.uni-saarland.de>
2013-12-13 20:11:36 +0100

    added printer tests for structs (flat and nested)

---
+++ test/resource/immanuel/printer-tests/struct-nested.print.c
@@ -0,0 +1,13 @@
+int foo()
+{
+	struct S
+	{
+		struct _S
+		{
+			char;
+			struct S (*s);
+			int x;
+		} _s;
+		void;
+	};
+}
---
+++ test/resource/immanuel/printer-tests/struct.print.c
@@ -0,0 +1,19 @@
+int foo()
+{
+	struct S;
+	struct S s;
+	struct
+	{
+		int;
+	};
+	struct
+	{
+		int;
+	} s;
+	struct
+	{
+		int x;
+		int y;
+		char a;
+	};
+}
