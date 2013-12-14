c4d314cb264dbe412e619a9d1d393ec9d09a983e
Immanuel Haffner <s9imhaff@stud.uni-saarland.de>
2013-12-14 18:48:38 +0100

    added duffs device to the printer tests (and it works!)

---
+++ test/resource/immanuel/printer-tests/duffs_device.print.c
@@ -0,0 +1,25 @@
+int foo()
+{
+	int n;
+	(n = ((count + 7) / 8));
+	switch ((count % 8)) {
+		do {
+			case 0:
+				((*to) = (*(from++)));
+			case 7:
+				((*to) = (*(from++)));
+			case 6:
+				((*to) = (*(from++)));
+			case 5:
+				((*to) = (*(from++)));
+			case 4:
+				((*to) = (*(from++)));
+			case 3:
+				((*to) = (*(from++)));
+			case 2:
+				((*to) = (*(from++)));
+			case 1:
+				((*to) = (*(from++)));
+		} while (((--n) > 0));
+	}
+}
