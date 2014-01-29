03faf8e8acb129e2db88dc7636b59adc1fa131b6
Immanuel Haffner <s9imhaff@stud.uni-saarland.de>
2014-01-29 12:02:12 +0100

    added tests for struct pointer return parameters

---
+++ test/resource/immanuel/parser-tests/struct_ptr_ret_param.parse.c
@@ -0,0 +1,9 @@
+0
+
+struct S;
+
+struct S * foo();
+
+struct P { int x, y; char z; } bar();
+
+struct Q { int x; char y; int z; } * foobar();
---
+++ test/resource/immanuel/printer-tests/struct_ptr_ret_param.in.print.c
@@ -0,0 +1,7 @@
+struct S;
+
+struct S * foo();
+
+struct P { int x, y; char z; } bar();
+
+struct Q { int x; char y; int z; } * foobar();
