27b2e91978a60fc8ce51bae66c581e632f19b3d0
Maximilian Harz <s9maharz@stud.uni-saarland.de>
2014-02-01 15:29:47 +0100

    fixed return value in declarators test; fixed bug in parser: couldn't parse int f( int (g)) before

--- test/resource/maximilian.harz/declarators.parse.c
+++ test/resource/maximilian.harz/declarators.parse.c
@@ -1,4 +1,4 @@
-
+0
 
 int main()
 {
@@ -9,5 +9,3 @@ int fun(int (f)(int, int *(void (x)()))) {
   return 1;
 }
 int fun2(int f(int, int *(void (x)())));
-int fun3(int f(int, int *(void)));
-int fun4(int f(int, int *(void)));
