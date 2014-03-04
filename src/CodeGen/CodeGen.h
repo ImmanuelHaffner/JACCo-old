03614b8806f813471a5b2de85f49abfe27259b44
Immanuel Haffner <s9imhaff@stud.uni-saarland.de>
2014-03-04 15:26:47 +0100

    introduced a JumpTarget that stores the break/continue targets for one loop. Added a stack of jump targets and corresponding access methods to the CodeGenFunction

--- src/CodeGen/CodeGen.h
+++ src/CodeGen/CodeGen.h
@@ -10,6 +10,8 @@
 #define C4_CODEGEN_H
 
 
+#include <utility>
+#include <vector>
 #include "llvm/IR/Module.h"                /* Module */
 #include "llvm/IR/Function.h"              /* Function */
 #include "llvm/IR/Constant.h"              /* Constant::getNullValue */
@@ -26,6 +28,20 @@ namespace C4
 {
   namespace CodeGen
   {
+    /// \brief Stores the break and continue targets for a loop.
+    struct JumpTarget
+    {
+      JumpTarget( llvm::BasicBlock * const breakTarget,
+          llvm::BasicBlock * const continueTarget )
+        : breakTarget(breakTarget), continueTarget(continueTarget)
+      {}
+
+      ~JumpTarget() {}
+
+      llvm::BasicBlock * const breakTarget;
+      llvm::BasicBlock * const continueTarget;
+    };
+
     /// \brief Holds the most important objects for code generation and offers
     /// routines for common code gen tasks.
     struct CodeGenFunction
@@ -37,6 +53,23 @@ namespace C4
 
       ~CodeGenFunction() {}
 
+      inline void pushJumpTarget( JumpTarget const target )
+      {
+        jumpTargets.push_back( target );
+      }
+
+      inline JumpTarget getCurrentJumpTarget() const
+      {
+        return jumpTargets.back();
+      }
+
+      inline JumpTarget popJumpTarget()
+      {
+        JumpTarget const t = jumpTargets.back();
+        jumpTargets.pop_back();
+        return t;
+      }
+
       private:
       /* The global context (only one needed) */
       LLVMContext &Context;
@@ -46,6 +79,9 @@ namespace C4
 
       /* An IR-Builder to output intermediate instructions or types. */
       IRBuilder<> Builder;
+
+      /* A stack of the break/continue targets for loops. */
+      std::vector< JumpTarget > jumpTargets;
     };
   } // end namespace CodeGen
 } // end namespace C4
