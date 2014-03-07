bcd1beca4083dd3bb1807cc97fb9029a23b9ff52
Immanuel Haffner <s9imhaff@stud.uni-saarland.de>
2014-03-07 16:15:44 +0100

    implemented stream operator and printing for sema types

---
+++ src/Sema/Type.cc
@@ -0,0 +1,88 @@
+//===--- Type.cc ----------------------------------------------------------===//
+//
+//  ~~~ The C4 Compiler ~~~
+//
+//  This file implements the semantic types.
+//
+//===----------------------------------------------------------------------===//
+
+
+#include "Type.h"
+
+
+using namespace C4;
+using namespace Sema;
+
+
+std::ostream & Sema::operator<<( std::ostream &out, Type const &type )
+{
+  type.print( out );
+  out << std::endl; // flush the out stream
+  return out;
+}
+
+std::ostream & Sema::operator<<( std::ostream &out,
+    Type const * const type )
+{
+  type->print( out );
+  return out;
+}
+
+void Type::dump() const
+{
+  this->print( std::cout );
+}
+
+void FuncType::print( std::ostream &out ) const
+{
+  this->retType->print( out );
+  out << " (";
+
+  auto it = params.begin();
+  if ( it != params.end() )
+  {
+    (*it)->print( out );
+    ++it;
+    for ( ; it != params.end(); ++it )
+    {
+      out << ", ";
+      (*it)->print( out );
+    }
+  }
+  out << ")";
+}
+
+void VoidType::print( std::ostream &out ) const
+{
+  out << "void";
+}
+
+void StructType::print( std::ostream &out ) const
+{
+  out << "struct {";
+
+  auto it = elements.begin();
+  if ( it != elements.end() )
+  {
+    it->second->print( out );
+    ++it;
+    for ( ; it != elements.end(); ++it )
+    {
+      out << ", ";
+      it->second->print( out );
+    }
+  }
+
+  out << "}";
+}
+
+void PtrType::print( std::ostream &out ) const
+{
+  out << "*";
+  this->innerType->print( out );
+}
+
+void BasicType::print( std::ostream &out ) const
+{
+  out << name;
+}
