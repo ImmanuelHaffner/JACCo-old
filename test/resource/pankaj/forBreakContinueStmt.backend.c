3cf8dadabcd6138e42405ed1870333896fa016d8
Pankaj Khanchandani <s9pakhan@stud.uni-saarland.de>
2014-03-08 14:35:42 +0100

    Add test if without else.

---
+++ test/resource/pankaj/forBreakContinueStmt.backend.c
@@ -0,0 +1,15 @@
+int main()
+{
+
+	int x;
+	int y;
+	for(x = 0;x < 5;x = x + 1)
+	{
+	  if(x == 3)
+	  {
+	    break;
+	  }
+	  y = x;
+	}
+	return y;
+}
---
+++ test/resource/pankaj/whileBreakContinue.backend.c
@@ -0,0 +1,16 @@
+int main()
+{
+
+	int x;
+	int y;
+	while(x < 5)
+	{
+	  if(x == 3)
+	  {
+	    break;
+	  }
+	  y = x;
+	  x = x + 1;
+	}
+	return y;
+}
