7faddac45be81eac61f82e9dfc1d5f9a7854b6e5
Pankaj Khanchandani <s9pakhan@stud.uni-saarland.de>
2014-03-08 14:17:50 +0100

    Add for, while test.

---
+++ test/resource/pankaj/forStmt.backend.c
@@ -0,0 +1,14 @@
+4
+
+
+int main()
+{
+	
+	int x;
+	int y;
+	for(x = 0;x < 5;x = x + 1)
+	{
+	  y = x;
+	}
+	return y;
+}	
---
+++ test/resource/pankaj/whileStmt.backend.c
@@ -0,0 +1,13 @@
+5
+
+
+int main()
+{
+	int x;
+	x = 0;
+	while(x < 5)
+	{
+		x = x + 1;
+	}
+	return x;
+}
