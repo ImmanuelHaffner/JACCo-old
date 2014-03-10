d9311a95b1a6e99430ea8ddb0ca6d21637cefaf3
Immanuel Haffner <s9imhaff@stud.uni-saarland.de>
2014-03-10 20:19:59 +0100

    implemented the join function for lattice values

--- src/Optimization/LatticeValue.cc
+++ src/Optimization/LatticeValue.cc
@@ -1,30 +1,55 @@
 #include "LatticeValue.h"
 
+
 using namespace C4;
 using namespace llvm;
 using namespace Optimize;
 
+
 bool LatticeValue::setTop()
 {
-  if ( type == TOP )
+  if ( type == VT::TOP )
     return false;
-  type = TOP;
+  type = VT::TOP;
   return true;
 }
 
-bool LatticeValue::setConstant( Constant * constant )
+bool LatticeValue::setConstant( Constant * const constant )
 {
-  if ( type == TOP )
+  if ( type == VT::TOP )
     return false;
 
-  if ( type == CONSTANT )
+  if ( type == VT::CONSTANT )
   {
     assert( getConstant() == constant &&
         "Value already set to another constant");
     return false;
   }
 
-  type = CONSTANT;
+  type = VT::CONSTANT;
+  this->constant = constant;
+  return true;
+}
+
+bool LatticeValue::join( llvm::Constant * const constant )
+{
+  /* TOP stays TOP. */
+  if ( VT::TOP == type )
+    return false;
+
+  /* If we join two constants.  If the values differ, we get TOP, otherwise
+   * nothing changes.
+   */
+  if ( VT::CONSTANT == type )
+  {
+    if ( this->constant == constant )
+      return false;
+
+    this->type = VT::TOP;
+    return true;
+  }
+
+  this->type = VT::CONSTANT;
   this->constant = constant;
   return true;
 }
--- src/Optimization/LatticeValue.h
+++ src/Optimization/LatticeValue.h
@@ -1,7 +1,9 @@
 #ifndef LATTICEVALUE_H
 #define LATTICEVALUE_H
 
+
 #include <llvm/IR/Constant.h>
+
 
 namespace C4
 {
@@ -10,28 +12,32 @@ namespace C4
     struct LatticeValue
     {
       LatticeValue() : type(BOTTOM) {}
-     /* Return true if status changes */
-      bool setConstant( llvm::Constant * );
-      bool setTop();
 
-      inline bool isConstant() { return type == CONSTANT; }
-      inline bool isTop() { return type == TOP; }
-      inline bool isBottom() { return type == BOTTOM; }
+      /* Return true if status changes */
+      bool setTop();
+      bool setConstant( llvm::Constant * const constant );
+      bool join( llvm::Constant * const constant );
+
+      inline bool isConstant() { return type == VT::CONSTANT; }
+      inline bool isTop() { return type == VT::TOP; }
+      inline bool isBottom() { return type == VT::BOTTOM; }
       inline llvm::Constant * getConstant() { return constant; }
 
       private:
-      enum ValueType
+      enum class VT
       {
         TOP,
         CONSTANT,
         BOTTOM
       };
-      
-      ValueType type;
+
+      VT type;
       /* The constant * if this is a constant */
-      llvm::Constant * constant;
+      llvm::Constant *constant = NULL;
     };
 
   }
 }
+
+
 #endif
