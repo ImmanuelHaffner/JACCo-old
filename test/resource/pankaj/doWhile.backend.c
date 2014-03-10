afab421b38ac6b6b258ec534cb79a37ea4fb09b5
Pankaj Khanchandani <s9pakhan@stud.uni-saarland.de>
2014-03-10 13:21:24 +0100

    Add doWhile testcase.

---
+++ test/resource/pankaj/doWhile.backend.c
@@ -0,0 +1,14 @@
+5
+
+
+int main()
+{
+  int x;
+  x = 0;
+  do
+  {
+    x = x + 1;
+  }while(x < 5);
+
+  return x;
+}
---
+++ test/resource/pankaj/doWhile2.backend.c
@@ -0,0 +1,14 @@
+11
+
+
+int main()
+{
+  int x;
+  x = 10;
+  do
+  {
+    x = x + 1;
+  }while(x < -1);
+
+  return x;
+}
