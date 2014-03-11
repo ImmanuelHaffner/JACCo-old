d81cd6ac1528787e859a1fc7461d235cb57c1915
Pankaj Khanchandani <s9pakhan@stud.uni-saarland.de>
2014-03-11 14:48:30 +0100

    Add test case.

---
+++ test/resource/pankaj/funPointerInStructDecl.sema.c
@@ -0,0 +1,36 @@
+0
+
+struct S
+{
+  char d;
+  int (*f)(int);
+  struct T
+  {
+    char b;
+    int (*g)(int);
+    int c;
+  } *pt;
+  struct T t;
+  int a;
+};
+
+int f(int x)
+{
+  return 20;
+}
+
+int g(int y)
+{
+  return 30;
+}
+int main()
+{
+  struct S s;
+  s.a = 10;
+  int (*h)(int);
+  h(0);
+  s.f = &f;
+  //s.pt = &s.t;
+
+  //return s.a + (s.f)(0) + (s.pt->g)(1);
+}
