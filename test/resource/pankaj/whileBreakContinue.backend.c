e0896b0672e08fb3b0b51adb7e3cc92c83e7b703
Immanuel Haffner <s9imhaff@stud.uni-saarland.de>
2014-03-11 19:18:38 +0100

    fixed backend test (missing initialization of variables)

--- test/resource/pankaj/whileBreakContinue.backend.c
+++ test/resource/pankaj/whileBreakContinue.backend.c
@@ -3,17 +3,20 @@
 
 int main()
 {
-
 	int x;
 	int y;
-	while(x < 5)
+
+  x = 0;
+  y = 0;
+
+	while ( x < 5 )
 	{
-	  if(x == 3)
-	  {
+	  if ( x == 3 )
 	    break;
-	  }
+
 	  y = x;
 	  x = x + 1;
 	}
+
 	return y;
 }
